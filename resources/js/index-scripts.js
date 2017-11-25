64a9185451ef3c98875cc4a27185a077eaf949c6
Fellipe <gurgelfellipe@gmail.com>
2017-11-25 16:16:19 -0200

    all files of the TCC app, copied from bitbucket project

---
+++ resources/js/index-scripts.js
@@ -0,0 +1,115 @@
+/**
+ * Created by Fellipe G on 11/11/2017.
+ */
+
+var restrictionNumber = 1;
+
+// Add restriction function: add a new div restriction to the list
+document.getElementById("add-restriction").onclick = function () {
+    var ok = true;
+
+    if (ok === true) {
+        var div = document.createElement('div');
+        div.className = 'restriction';
+        div.id = 'restriction'+restrictionNumber.toString();
+        div.setAttribute('onchange', 'redefineGraph(this)');
+        div.setAttribute('value', restrictionNumber);
+
+        var x1Input = document.createElement('input');
+        x1Input.id = 'x1-'.concat(restrictionNumber.toString());
+        x1Input.className = 'coefficient';
+        x1Input.typeName = 'text';
+
+        var x1Label = document.createElement('a');
+        x1Label.innerText = ' x1 ';
+
+        var operatorSelect = document.createElement('select');
+        operatorSelect.className = 'btn-dark';
+        operatorSelect.name = 'arithmetic-operator';
+        var plus = document.createElement('option');
+        plus.innerText = '+';
+        plus.value = "plus";
+        var minus = document.createElement('option');
+        minus.innerText = '-';
+        minus.value = "minus";
+        operatorSelect.appendChild(plus);
+        operatorSelect.appendChild(minus);
+
+        div.appendChild(x1Input);
+        div.appendChild(x1Label);
+        div.appendChild(operatorSelect);
+
+        var x2Input = document.createElement('input');
+        x2Input.id = 'x2-'.concat(restrictionNumber.toString());
+        x2Input.className = 'coefficient';
+        x2Input.typeName = 'text';
+        x2Input.style = 'margin-left: 5px';
+
+        var x2Label = document.createElement('a');
+        x2Label.innerText = ' x2 ';
+
+        var comparisonOperatorSelect = document.createElement('select');
+        comparisonOperatorSelect.className = 'btn-dark';
+        comparisonOperatorSelect.name = "comparison-operator";
+
+        var greaterOrEqual = document.createElement('option');
+        greaterOrEqual.innerText = '>=';
+        greaterOrEqual.value = 'greater-or-equal';
+
+        var lessOrEqual = document.createElement('option');
+        lessOrEqual.innerText = '<=';
+        lessOrEqual.value = 'less-or-equal';
+
+        var equal = document.createElement('option');
+        equal.innerText = '=';
+        equal.value = 'equal';
+
+        comparisonOperatorSelect.appendChild(lessOrEqual);
+        comparisonOperatorSelect.appendChild(greaterOrEqual);
+        comparisonOperatorSelect.appendChild(equal);
+
+        var restrictionValue = document.createElement('input');
+        restrictionValue.id = 'result-'.concat(restrictionNumber.toString());
+        restrictionValue.className = 'coefficient';
+        restrictionValue.typeName = 'text';
+        restrictionValue.style = 'margin-left: 4px';
+
+        var removeButton = document.createElement('button');
+        removeButton.className = 'btn btn-danger remove-btn';
+        removeButton.setAttribute('onclick', 'removeRestriction("'+restrictionNumber.toString()+'")');
+        removeButton.innerHTML = '<a data-toggle="tooltip" data-placement="right" title="Remover esta restrição"> X </a>';
+
+        div.appendChild(x2Input);
+        div.appendChild(x2Label);
+        div.appendChild(comparisonOperatorSelect);
+        div.appendChild(restrictionValue);
+        div.appendChild(removeButton);
+
+        var restrictionComment = document.createComment(' RESTRICTION '.concat(restrictionNumber.toString()).concat(' '));
+        var restrictionContainer = document.getElementById('restrictions-container');
+        restrictionContainer.appendChild(restrictionComment);
+        restrictionContainer.appendChild(div);
+        restrictionNumber++;
+
+        tooltip();
+    }
+};
+
+function removeRestriction(restrictionNumber) {
+    var restrictionToBeRemoved = document.getElementById('restriction'+restrictionNumber);
+    restrictionToBeRemoved.remove();
+    hideTooltip();
+    removeLine(restrictionNumber);
+};
+
+// Tooltip functions
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+function tooltip() {
+    $('[data-toggle="tooltip"]').tooltip();
+}
+function hideTooltip() {
+    $('[role="tooltip"]').remove();
+}
+
---
+++ resources/js/resolution-graph.js
@@ -0,0 +1,244 @@
+/**
+ * Created by Fellipe G on 11/5/2017.
+ */
+
+/** CONTROL VARIABLES **/
+// setting up the size of the graph
+var margin = {top: 20, right: 100, bottom: 30, left: 70},
+    width = 920 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+// MAX NUMBERS ON AXIS
+var maxXscale = 10;
+var maxYscale = 10;
+// DEFINING THE SCALE: NUMBERS THAT WILL APPEAR UNDER/BESIDE THE AXIS
+var xScale = d3.scale.linear()
+    .domain([0, maxXscale])
+    .range([0, width]);
+var yScale = d3.scale.linear()
+    .domain([0, maxYscale])
+    .range([height, 0]);
+// ARRAY OF COLORS, LIMITED IN 12 COLORS
+var colorsArray = ["RoyalBlue",
+                   "Chartreuse",
+                   "BlueViolet",
+                   "Tomato",
+                   "Navy",
+                   "Brown",
+                   "DarkCyan",
+                   "DarkGoldenRod",
+                   "DarkOrange",
+                   "Fuchsia",
+                   "Gold",
+                   "Black"];
+var colorsArrayLenght = colorsArray.length;
+var scalesDatasetArray = [];
+var linesArray = [];
+/** END OF CONTROL VARIABLES **/
+
+// DRAWING THE DEFAULT GRAPH
+drawGraph();
+
+// DRAWS THE GRAPH AREA AND ITS AXIS
+function drawGraph() {
+
+    // CREATING THE AXIS LINES WITH THE SCALE RANGE ABOVE
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom")
+        .innerTickSize(-height) // changes the bar grids
+        .outerTickSize(1)
+        .tickPadding(10);
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left")
+        .innerTickSize(-width) // changes the bar grids
+        .outerTickSize(1)
+        .tickPadding(10); // how far from the line, the scale will appear
+
+    // REMOVING THE "ALREADY THERE" GRAPH IN ORDER TO REPLACE IT AND NOT ADD ANOTHER ONE
+    var svg = d3.select("#graph");
+    if (svg[0][0] !== null) {
+        svg.remove("#graph-child");
+    }
+    svg = d3.select("body").append("svg")
+        .attr("id", "graph")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom+20)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("id", "graph-child");
+
+    // APPENDING THE X AXIS TO THE GRAPH
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("text")
+        .attr("transform",
+            "translate(" + (width/2) + " ," +
+            (height + margin.top + 20) + ")")
+        .attr("class", "label")
+        .text("x1");
+
+    // APPENDING THE Y AXIS TO THE GRAPH
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    // text label for the y axis
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "label")
+        .text("x2");
+}
+
+// UPDATES THE SCALE
+function updateGraphScale(){
+
+    var x = d3.max(scalesDatasetArray, function (d) {
+        return d.x;
+    });
+
+    var y = d3.max(scalesDatasetArray, function (d) {
+        return d.y;
+    });
+
+    maxXscale = x;
+    maxYscale = y;
+
+    yScale = d3.scale.linear()
+        .domain([0, maxYscale])
+        .range([height, 0]);
+
+    xScale = d3.scale.linear()
+        .domain([0, maxXscale])
+        .range([0, width]);
+}
+
+// DRAWS A LINE ON THE GRAPH
+function drawLine(dataset, restrictionNumber, svg) {
+
+    var lineDataset = [{x: dataset[0].x, y:dataset[0].y},{
+                       x: dataset[1].x, y:dataset[1].y}];
+
+    if (lineDataset[1].x === -1)
+        lineDataset[1].x = maxXscale;
+
+    if (lineDataset[1].y === -1)
+        lineDataset[1].y = maxYscale;
+
+    // CREATING THE LINE ON THE GRAPH CONSIDERING THE AXIS' SCALES
+    var line = d3.svg.line()
+        .x(function (d) {
+            return xScale(d.x);
+        })
+        .y(function (d) {
+            return yScale(d.y);
+        });
+
+    // APPENDING THE LINE TO THE SVG GRAPH
+    svg.append("path")
+        .data([lineDataset])
+        .attr("class", "line")
+        .attr("d", line)
+        .attr("stroke", setLineColor(restrictionNumber));
+}
+
+function setLineColor(restrictionNumber) {
+    var colorIndex = restrictionNumber%colorsArrayLenght;
+    return colorsArray[colorIndex];
+}
+
+// REDRAWS THE WHOLE GRAPH BASED ON THE CONSTRAINTS EQUATIONS
+function redefineGraph(restriction) {
+
+    // GETTING THE INPUTS
+    var inputs = restriction.getElementsByClassName('coefficient');
+    var x1 = parseInt(inputs[0].value) || 0;
+    var x2 = parseInt(inputs[1].value) || 0;
+    var res = parseInt(inputs[2].value) || 0;
+    var arithmeticOperator = restriction.getElementsByTagName('select')[0];
+    var restrictionNumber = parseInt(restriction.getAttribute('value'));
+    var scaleDataset;
+    var lineDataset;
+
+    // DEFINING THE BOTH LINE AND SCALE DATA SETS
+    if(res === 0){
+        return;
+    }
+    if(x1 !== 0){
+        // both x1 and x2 set
+        if(x2 !== 0){
+            scaleDataset = {x: Math.ceil(res/x1), y: Math.ceil(res/x2)};
+            scalesDatasetArray[restrictionNumber] = scaleDataset;
+            updateGraphScale(scalesDatasetArray);
+
+            lineDataset = [
+                {x: 0, y: Math.ceil(res/x2)},
+                {x: Math.ceil(res/x1), y: 0}
+            ];
+
+        // only x1 set
+        } else {
+            scaleDataset = {x: Math.ceil(res/x1), y: Math.ceil(res/x1)};
+            scalesDatasetArray[restrictionNumber] = scaleDataset;
+            updateGraphScale(scalesDatasetArray);
+
+            lineDataset = [
+                {x: res/x1, y: 0},
+                {x: res/x1, y: -1}
+            ];
+        }
+    } else {
+        // only x2 set
+        if(x2 !== 0){
+            scaleDataset = {x: Math.ceil(res/x2), y: Math.ceil(res/x2)};
+            scalesDatasetArray[restrictionNumber] = scaleDataset;
+            updateGraphScale(scalesDatasetArray);
+
+            lineDataset = [
+                {x: 0, y: res/x2},
+                {x: -1, y: res/x2}
+            ];
+        } else {
+            lineDataset = [
+                {x: 0, y: 0},
+                {x: 0, y: 0}
+            ];
+        }
+    }
+
+    // DRAWING THE WHOLE GRAPH AREA AGAIN WITH UPDATED SCALES
+    drawGraph();
+
+    // ADDING THE NEW LINE TO THE ARRAY OF LINES
+    linesArray[restrictionNumber] = lineDataset;
+
+    // DRAWING ALL THE LINES BASED NOW ON THE NEW SCALE
+    for (i = 0 ; i < linesArray.length ; i++) {
+        drawLine(linesArray[i], i, d3.select('#graph-child'));
+    }
+}
+
+// REMOVES A LINE FROM THE GRAPH AREA
+function removeLine(restrictionNumber){
+    resetLineDataset = [{x: 0, y: 0},
+                        {x: 0, y: 0}];
+    resetScaleDataset = [{x: 0, y: 0}];
+
+    linesArray[restrictionNumber] = resetLineDataset;
+    scalesDatasetArray[restrictionNumber] = resetScaleDataset;
+
+    updateGraphScale();
+
+    drawGraph();
+
+    for (i = 0 ; i < linesArray.length ; i++) {
+        drawLine(linesArray[i], i, d3.select('#graph-child'));
+    }
+}
