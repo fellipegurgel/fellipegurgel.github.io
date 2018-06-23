b4c18c3c031afd350a77b9e3e13f4649afaa3524
Fellipe <gurgelfellipe@gmail.com>
2018-06-23 19:03:20 -0300

    intersection points calculation and tests;

--- resources/js/resolution-graph.js
+++ resources/js/resolution-graph.js
@@ -1,15 +1,17 @@
-/**
- * Created by Fellipe G on 11/5/2017.
- */
+/************************************************
+ ** Script to draw/render the resolution graph **
+ ************************************************/
 
 /** CONTROL VARIABLES **/
-// setting up the size of the graph
+// SETTING UP GRAPH SIZE
 var margin = {top: 20, right: 100, bottom: 30, left: 70},
     width = 920 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-// MAX NUMBERS ON AXIS
+
+// DEFAULT MAX NUMBERS ON AXIS
 var maxXscale = 10;
 var maxYscale = 10;
+
 // DEFINING THE SCALE: NUMBERS THAT WILL APPEAR UNDER/BESIDE THE AXIS
 var xScale = d3.scale.linear()
     .domain([0, maxXscale])
@@ -17,6 +19,13 @@ var xScale = d3.scale.linear()
 var yScale = d3.scale.linear()
     .domain([0, maxYscale])
     .range([height, 0]);
+
+// CONTROL ARRAYS
+var graphScalesArray = [];
+var linesArray = [];
+var constraintsArray = [];
+var intersectionPointsArray = [];
+
 // ARRAY OF COLORS, LIMITED IN 12 COLORS
 var colorsArray = ["RoyalBlue",
                    "Crimson",
@@ -31,11 +40,9 @@ var colorsArray = ["RoyalBlue",
                    "Gold",
                    "Black"];
 var colorsArrayLenght = colorsArray.length;
-var scalesDatasetArray = [];
-var linesArray = [];
 /** END OF CONTROL VARIABLES **/
 
-// DRAWING THE DEFAULT GRAPH
+// DRAWING THE DEFAULT GRAPH (WHEN PAGE IS INITIALLY LOADED)
 drawGraph();
 
 // DRAWS THE GRAPH AREA AND ITS AXIS
@@ -97,14 +104,14 @@ function drawGraph() {
         .text("x2");
 }
 
-// UPDATES THE SCALE
+// UPDATES THE SCALE (WHEN THERE ARE CHANGES ON CONSTRAINTS)
 function updateGraphScale(){
 
-    var x = d3.max(scalesDatasetArray, function (d) {
+    var x = d3.max(graphScalesArray, function (d) {
         return d.x;
     });
 
-    var y = d3.max(scalesDatasetArray, function (d) {
+    var y = d3.max(graphScalesArray, function (d) {
         return d.y;
     });
 
@@ -153,75 +160,95 @@ function drawLine(dataset, restrictionNumber, svg) {
         .attr("d", line)
         .attr("stroke", setLineColor(restrictionNumber));
 
-    console.log("line dataset >> ");
-    console.log(lineDataset);
+    // TESTES PINTAR AREA
+    // console.log("line dataset >> ");
+    // console.log(lineDataset);
 
     // PAINTING THE AREA
     var teste = [{x: 35, y: 25}, {x: 35, y: 0}, {x: 50, y: 25}];
     var teste2 = [{x: 25, y: 15}, {x: 40, y: 15}, {x: 25, y: 0}, {x: 100, y: 0}];
     svg.append("path")
-        .datum(teste)
+        .datum(lineDataset)
         .attr("class", "area")
         .attr("d", area);
 }
 
+
+// SETS A COLOR TO A LINE ACCORDING TO ITS CONSTRAINT NUMBER
 function setLineColor(restrictionNumber) {
     var colorIndex = restrictionNumber%colorsArrayLenght;
     return colorsArray[colorIndex];
 }
 
-// REDRAWS THE WHOLE GRAPH BASED ON THE CONSTRAINTS EQUATIONS
+// REDRAWS THE WHOLE GRAPH BASED ON CONSTRAINTS CHANGES
 function redefineGraph(restriction) {
 
     // GETTING THE INPUTS
     var inputs = restriction.getElementsByClassName('coefficient');
     var x1 = parseInt(inputs[0].value) || 0;
     var x2 = parseInt(inputs[1].value) || 0;
-    var res = parseInt(inputs[2].value) || 0;
+    var limitValue = parseInt(inputs[2].value) || 0;
     var arithmeticOperator = restriction.getElementsByTagName('select')[0];
     var restrictionNumber = parseInt(restriction.getAttribute('value'));
     var scaleDataset;
     var lineDataset;
 
-    // DEFINING THE BOTH LINE AND SCALE DATA SETS
-    if(res === 0){
+    // IF THE LIMITATION VALUE IS NOT SET, ABORT
+    if(limitValue === 0){
         return;
     }
+
+    // SAVING THE CONSTRAINT COEFFICIENTS INTO AN ARRAY
+    var constraintObject = {
+        number: restrictionNumber,
+        x1: x1,
+        x2: x2,
+        limit: limitValue
+    };
+
+    // DEFINING LINE AND SCALE DATA SETS AND THE CONSTRAINT TYPE (ACCORDING TO ITS COEFFICIENTS)
     if(x1 !== 0){
-        // both x1 and x2 set
+        /** BOTH x1 AND x2 ARE SET **/
         if(x2 !== 0){
-            scaleDataset = {x: Math.ceil(res/x1), y: Math.ceil(res/x2)};
-            scalesDatasetArray[restrictionNumber] = scaleDataset;
-            updateGraphScale(scalesDatasetArray);
+            //
+            scaleDataset = {x: Math.ceil(limitValue/x1), y: Math.ceil(limitValue/x2)};
+            graphScalesArray[restrictionNumber] = scaleDataset;
+            updateGraphScale(graphScalesArray);
 
             lineDataset = [
-                {x: 0, y: Math.ceil(res/x2)},
-                {x: Math.ceil(res/x1), y: 0}
+                {x: 0, y: Math.ceil(limitValue/x2)},
+                {x: Math.ceil(limitValue/x1), y: 0}
             ];
 
-        // only x1 set
-        } else {
-            scaleDataset = {x: Math.ceil(res/x1), y: Math.ceil(res/x1)};
-            scalesDatasetArray[restrictionNumber] = scaleDataset;
-            updateGraphScale(scalesDatasetArray);
+            constraintObject.type = 1;
+
+        } else { /** ONLY x1 IS SET **/
+            scaleDataset = {x: Math.ceil(limitValue/x1), y: Math.ceil(limitValue/x1)};
+            graphScalesArray[restrictionNumber] = scaleDataset;
+            updateGraphScale(graphScalesArray);
 
             lineDataset = [
-                {x: res/x1, y: 0},
-                {x: res/x1, y: -1}
+                {x: limitValue/x1, y: 0},
+                {x: limitValue/x1, y: -1}
             ];
+
+            constraintObject.type = 2;
         }
     } else {
-        // only x2 set
+        /** ONLY x2 IS SET **/
         if(x2 !== 0){
-            scaleDataset = {x: Math.ceil(res/x2), y: Math.ceil(res/x2)};
-            scalesDatasetArray[restrictionNumber] = scaleDataset;
-            updateGraphScale(scalesDatasetArray);
+            scaleDataset = {x: Math.ceil(limitValue/x2), y: Math.ceil(limitValue/x2)};
+            graphScalesArray[restrictionNumber] = scaleDataset;
+            updateGraphScale(graphScalesArray);
 
             lineDataset = [
-                {x: 0, y: res/x2},
-                {x: -1, y: res/x2}
+                {x: 0, y: limitValue/x2},
+                {x: -1, y: limitValue/x2}
             ];
-        } else {
+
+            constraintObject.type = 3;
+
+        } else { /** BOTH x1 AND x2 ARE SET WITH ZERO **/
             lineDataset = [
                 {x: 0, y: 0},
                 {x: 0, y: 0}
@@ -229,26 +256,118 @@ function redefineGraph(restriction) {
         }
     }
 
-    // DRAWING THE WHOLE GRAPH AREA AGAIN WITH UPDATED SCALES
-    drawGraph();
-
     // ADDING THE NEW LINE TO THE ARRAY OF LINES
     linesArray[restrictionNumber] = lineDataset;
+
+    // ADDING THE NEW CONSTRAINT TO THE ARRAY OF CONSTRAINTS
+    constraintsArray[restrictionNumber] = constraintObject;
+
+    // DRAWING THE WHOLE GRAPH AREA AGAIN WITH UPDATED SCALES
+    drawGraph();
 
     // DRAWING ALL THE LINES BASED NOW ON THE NEW SCALE
     for (i = 0 ; i < linesArray.length ; i++) {
         drawLine(linesArray[i], i, d3.select('#graph-child'));
     }
+
+    // CALCULATING THE CONCURRENT POINTS
+    calculateIntersectionPoints(constraintsArray);
+
+    console.log("intersection points: ")
+    console.log(intersectionPointsArray);
 }
 
+// BASED ON THE CONSTRAINTS EQUATIONS, FIND ALL THE INTERSECTION POINTS, IF EXISTS
+function calculateIntersectionPoints(constraintsArray){
+    var i, j = 0;
+    var constraintsArrayLength = constraintsArray.length;
+    var equation1, equation2;
+    var intersectionPointObject = {
+        x: 0,
+        y: 0,
+        objectiveFunctionResult: 0
+    };
+
+    for(i = 0; i < constraintsArrayLength-1; i++){
+        for(j = i+1; j < constraintsArrayLength; j++){
+
+            // SINCE ORDER MATTERS HERE, IT HAS TO MAKE SURE THAT THE EQUATION-1
+            // IS THE ONE WITH MORE COEFFICIENTS
+            if(constraintsArray[i].type === 2 || constraintsArray[i].type === 3) {
+                equation1 = constraintsArray[j];
+                equation2 = constraintsArray[i];
+                // IF THE EQUATIONS HAVE THE SAME TYPE, THERE IS NO INTERSECTION POINT
+                if(equation1.type === equation2.type){
+                    break;
+                }
+            } else {
+                equation1 = constraintsArray[i];
+                equation2 = constraintsArray[j];
+            }
+
+            // IF ONE OF THE EQUATIONS HAVE ONLY ONE COEFFICIENT SET,
+            // IT MUST FIX EITHER X OR Y OF THE INTERSECTION POINT
+            if(equation2.type === 2){
+                intersectionPointObject.x = equation2.limit/equation2.x1;
+            } else if (equation2.type === 3){
+                intersectionPointObject.y = equation2.limit/equation2.x2;
+            }
+            if(equation1.type === 2){
+                intersectionPointObject.x = equation1.limit/equation1.x1;
+            } else if (equation1.type === 3){
+                intersectionPointObject.y = equation1.limit/equation1.x2;
+            }
+
+            // IF BOTH X AND Y OF THE INTERSECTION POINT ARE ALREADY DEFINED,
+            // FOLLOW THE BAILE
+            if (intersectionPointObject.x !== 0 && intersectionPointObject.y !== 0){
+                intersectionPointsArray.push(intersectionPointObject);
+                break;
+            }
+
+            /** HERE, IT'S SURE THAT THE EQUATION-1 IS OF TYPE 1 **/
+            /*
+             Since we have two equations like:
+             y = a1 * x + b1
+             y = a2 * x + b2
+             Como queremos verificar a intersecção, fazemos y(reta1) = y(reta2) onde teremos:
+             a1 * x + b1 = a2 * x + b2
+             Isolando o valor de x temos:
+             x = ( b2 - b1 ) / (a1 - a2)
+             Se a1 == a2 teremos um erro (divisão por zero), o que indica que as retas são paralelas (não têm intersecção).
+             Utilizaremos o valor de x em alguma das equações acima:
+             y = a1 * x + b1
+             */
+            var a1 = -equation1.x1/equation1.x2;
+            var b1 = equation1.limit/equation1.x2;
+
+            if (equation2.type === 2){
+                intersectionPointObject.y = a1*intersectionPointObject.x + b1;
+            } else if (equation2.type === 3){
+                intersectionPointObject.x = (intersectionPointObject.y - b1)/a1;
+            } else {
+                var a2 = -equation2.x1/equation2.x2;
+                var b2 = equation2.limit/equation2.x2;
+                intersectionPointObject.x = ( b2-b1 ) / ( a1-a2 );
+                intersectionPointObject.y = a1*intersectionPointObject.x + b1;
+            }
+
+            intersectionPointsArray.push(intersectionPointObject);
+        }
+    }
+}
+
+
+
+// TODO: RENAME IT TO REMOVE CONSTRAINT
 // REMOVES A LINE FROM THE GRAPH AREA
 function removeLine(restrictionNumber){
-    resetLineDataset = [{x: 0, y: 0},
+    var resetLineDataset = [{x: 0, y: 0},
                         {x: 0, y: 0}];
-    resetScaleDataset = [{x: 0, y: 0}];
+    var resetScaleDataset = [{x: 0, y: 0}];
 
     linesArray[restrictionNumber] = resetLineDataset;
-    scalesDatasetArray[restrictionNumber] = resetScaleDataset;
+    graphScalesArray[restrictionNumber] = resetScaleDataset;
 
     updateGraphScale();
 
